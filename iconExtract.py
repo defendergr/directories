2bfb9c7fc6f4d0d48d93da682a3ed2c70221cd90
Κωνσταντίνος Καρακασίδης <k.karakasidis@dimos.thermis.local>
2022-12-27 14:18:50 +0200

    Τρι 27/12/2022 script save

--- iconExtract.py
+++ iconExtract.py
@@ -9,8 +9,14 @@ from PIL import Image
 class IconExtract():
 
     def __init__(self):
-        self.pattern = r'./Setup/**/*.exe'
-        self.fileList = glob.glob(self.pattern, recursive=True)
+        self.patternEXE = r'./Setup/**/*.exe'
+        self.fileListEXE = glob.glob(self.patternEXE, recursive=True)
+
+        self.patternMSI = r'./Setup/**/*.msi'
+        self.fileListMSI = glob.glob(self.patternMSI, recursive=True)
+
+        self.fileList = self.fileListEXE + self.fileListMSI
+
         self.pathToSaveIcons = r'./icons'
         if not os.path.exists(self.pathToSaveIcons):
             os.mkdir(self.pathToSaveIcons)
@@ -20,28 +26,31 @@ class IconExtract():
 
     def extract_icon_from_exe(self, icon_in_path, icon_name, icon_out_path, size=120):
 
+        try:
+            ico_x = win32api.GetSystemMetrics(win32con.SM_CXICON)
+            ico_y = win32api.GetSystemMetrics(win32con.SM_CYICON)
 
-        ico_x = win32api.GetSystemMetrics(win32con.SM_CXICON)
-        ico_y = win32api.GetSystemMetrics(win32con.SM_CYICON)
+            large, small = win32gui.ExtractIconEx(icon_in_path, 0)
+            win32gui.DestroyIcon(small[0])
 
-        large, small = win32gui.ExtractIconEx(icon_in_path,0)
-        win32gui.DestroyIcon(small[0])
+            hdc = win32ui.CreateDCFromHandle(win32gui.GetDC(0))
+            hbmp = win32ui.CreateBitmap()
+            hbmp.CreateCompatibleBitmap(hdc, ico_x, ico_y)
+            hdc = hdc.CreateCompatibleDC()
 
-        hdc = win32ui.CreateDCFromHandle( win32gui.GetDC(0) )
-        hbmp = win32ui.CreateBitmap()
-        hbmp.CreateCompatibleBitmap( hdc, ico_x, ico_y )
-        hdc = hdc.CreateCompatibleDC()
+            hdc.SelectObject(hbmp)
+            hdc.DrawIcon((0, 0), large[0])
 
-        hdc.SelectObject( hbmp )
-        hdc.DrawIcon( (0,0), large[0] )
+            bmpstr = hbmp.GetBitmapBits(True)
+            icon = Image.frombuffer('RGBA', (32, 32), bmpstr, 'raw', 'BGRA', 0, 1)
 
-        bmpstr = hbmp.GetBitmapBits(True)
-        icon = Image.frombuffer('RGBA', (32,32), bmpstr, 'raw', 'BGRA', 0, 1)
+            full_outpath = os.path.join(icon_out_path, "{}.png".format(icon_name))
+            # print(icon_name)
+            icon.resize((size, size))
+            icon.save(full_outpath)
+        except:
+            full_outpath = './images/msi.ico'
 
-        full_outpath = os.path.join(icon_out_path, "{}.png".format(icon_name))
-        # print(icon_name)
-        icon.resize((size, size))
-        icon.save(full_outpath)
         #return the final path to the image
         return full_outpath
 
@@ -60,9 +69,7 @@ class IconExtract():
             fileInstall['img'] = name[-1]
             fileInstall['path'] = f
             self.filesDict.append(fileInstall)
-        # print(self.numberOfSetupFiles)
-        # for i in self.filesDict:
-            # print(i)
+
         return self.filesDict
 
 
